57e89a43e718ca2a5dcdfad62483610551220015
kobecarter <zakaria.hab@gmail.com>
2026-07-14 15:20:18 +0100

    Replace Google Tag Manager with direct GA4 gtag.js snippet

--- includes/template.php
+++ includes/template.php
@@ -7,24 +7,15 @@
 
 	    <meta name="website" content="<?php echo $siteURL; ?>">
 	    <?php getSeoMeta($_GET); ?>
-  <!-- Google Tag Manager -->
-	    <script>
-	    (function(w, d, s, l, i) {
-	        w[l] = w[l] || [];
-	        w[l].push({
-	            'gtm.start': new Date().getTime(),
-	            event: 'gtm.js'
-	        });
-	        var f = d.getElementsByTagName(s)[0],
-	            j = d.createElement(s),
-	            dl = l != 'dataLayer' ? '&l=' + l : '';
-	        j.async = true;
-	        j.src =
-	            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-	        f.parentNode.insertBefore(j, f);
-	    })(window, document, 'script', 'dataLayer', 'GTM-KN2KF6H8');
-	    </script>
-	    <!-- End Google Tag Manager -->
+      <!-- Google tag (gtag.js) -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-V6N5Y8QJ1M"></script>
+      <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-V6N5Y8QJ1M');
+      </script>
 	   
 
 	    <!-- Meta Pixel Code -->
